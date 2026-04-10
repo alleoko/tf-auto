06a0c9254975f726915454756ab3b789d1533705
alejo len aclan <61332372+alleoko@users.noreply.github.com>
2026-04-10 20:16:30 +0800

    Add files via upload

---
+++ tf-infra/vpc.tf
@@ -0,0 +1,25 @@
+###############################################################################
+# vpc.tf
+###############################################################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${var.app_name}-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
+  public_subnets  = var.public_subnet_cidrs
+  private_subnets = var.private_subnet_cidrs
+
+ # To enable NAT, Make it true
+  enable_nat_gateway   = false
+  single_nat_gateway   = false
+  #enable_nat_gateway   = true
+  #single_nat_gateway   = true
+  
+  enable_dns_hostnames = true
+
+  tags = local.common_tags
+}
