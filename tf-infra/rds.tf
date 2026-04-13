d8d33cb3a7f464781d3787391c89462a7d33d364
alleoko <allenaclan27@gmail.com>
2026-04-13 14:02:10 +0800

    config js mod

--- tf-infra/rds.tf
+++ tf-infra/rds.tf
@@ -2,6 +2,54 @@
 # rds.tf  –  RDS PostgreSQL shared by all API services
 ###############################################################################
 
+#resource "aws_db_subnet_group" "main" {
+#  name       = "${var.app_name}-db-subnet-group"
+#  subnet_ids = module.vpc.private_subnets
+# tags       = local.common_tags
+#}
+
+#resource "aws_db_instance" "postgres" {
+#  identifier        = "${var.app_name}-postgres"
+#  engine            = "postgres"
+#  engine_version    = "16"
+#  instance_class    = var.db_instance_class
+#  db_name           = var.db_name
+#  username          = var.db_username
+#  password          = var.db_password
+
+#  db_subnet_group_name   = aws_db_subnet_group.main.name
+#  vpc_security_group_ids = [aws_security_group.rds.id]
+
+#  allocated_storage       = 20
+#  max_allocated_storage   = 100
+#  storage_type            = "gp2"
+#  storage_encrypted       = true
+#  backup_retention_period = 7
+#  skip_final_snapshot     = true
+#  deletion_protection     = false
+#  publicly_accessible     = false
+#  multi_az                = false
+
+#  tags = local.common_tags
+#}
+
+###############################################################################
+# rds.tf  –  RDS PostgreSQL shared by all API services
+###############################################################################
+
+resource "aws_db_parameter_group" "postgres" {
+  name   = "${var.app_name}-postgres-params"
+  family = "postgres16"
+
+  parameter {
+    name         = "rds.force_ssl"
+    value        = "0"
+    apply_method = "pending-reboot"
+  }
+
+  tags = local.common_tags
+}
+
 resource "aws_db_subnet_group" "main" {
   name       = "${var.app_name}-db-subnet-group"
   subnet_ids = module.vpc.private_subnets
@@ -19,6 +67,7 @@ resource "aws_db_instance" "postgres" {
 
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.rds.id]
+  parameter_group_name   = aws_db_parameter_group.postgres.name
 
   allocated_storage       = 20
   max_allocated_storage   = 100
